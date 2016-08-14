a072972844df7ddf2f64043910e25b112aced49b
Danilo Bargen <mail@dbrgn.ch>
2016-08-14 17:51:55 +0200

    Initial project setup

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# librepcb-api
+# LibrePCB API
+
+The API is implemented with Django REST Framework on Python 3.
+
+## Setup
+
+Create a virtualenv:
+
+    pyvenv VIRTUAL
+
+Export some environment variables:
+
+    echo 'export DJANGO_DEBUG=true' >> VIRTUAL/bin/activate
+    echo 'export DATABASE_URL=postgres:///librepcb-api' >> VIRTUAL/bin/activate
+
+Enable virtualenv:
+
+    source VIRTUAL/bin/activate
+
+Install dependencies:
+
+    pip install -r requirements.txt
+
+Create a database:
+
+    createdb librepcb-api
+
+Initialize database:
+
+    src/manage.py migrate
+
+Start the development server:
+
+    src/manage.py runserver
+
+The server is now running on `http://localhost:8000/`.
+
+## License
+
+GPLv3, see `LICENSE` file.
---
+++ src/config/settings.py
@@ -0,0 +1,115 @@
+"""
+Django settings for librepcb_api project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.8/ref/settings/
+"""
+
+import os
+
+from django.core.exceptions import ImproperlyConfigured
+
+import dj_database_url
+
+
+# Useful helpers to query environment variables
+env = os.environ.get
+true_values = ['1', 'true', 'y', 'yes', 1, True]
+
+
+def require_env(name):
+    value = env(name)
+    if not value:
+        raise ImproperlyConfigured('Missing {} environment variable'.format(name))
+    return value
+
+
+# Base directory
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY', 'debug')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env('DJANGO_DEBUG', 'false').lower() in true_values
+
+ALLOWED_HOSTS = []
+
+ADMIN_EMAIL = env('ADMIN_EMAIL')
+if ADMIN_EMAIL:
+    ADMINS = [('Admin', ADMIN_EMAIL)]
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+)
+
+ROOT_URLCONF = 'config.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'config.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+
+DATABASES = {
+    'default': dj_database_url.config(),
+}
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.8/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
